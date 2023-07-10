cbfcdddad5516b2074ee9346505716f75ded0f59
Katherine de Vay <katdevay2@gmail.com>
2023-07-10 11:30:35 -0500

    Debug mongoose dependency issue

--- api/src/job/jobs.module.ts
+++ api/src/job/jobs.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { JobsController } from './Jobs.controller';
 import { JobsService } from './jobs.service';
+import { JobSchema, Job } from './schemas/job.schema';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: Job.name, schema: JobSchema }])],
   controllers: [JobsController],
   providers: [JobsService],
 })
